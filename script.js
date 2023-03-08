3df51b1ceb2cbbae0896cb624d9c45e7e3ead186
Yan Torres <yantmartins@live.com>
2023-03-07 23:35:51 -0400

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function somar(){
+    let n1 = document.getElementById("n1").value
+    let n2 = document.getElementById("n2").value
+
+    const soma = parseFloat(n1) + parseFloat(n2)
+
+    alert(soma)
+}
+
+class Calculadora {
+    constructor (){
+        
+    }
+}
